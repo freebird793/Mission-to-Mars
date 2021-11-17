3a8c4c878bf9f7417d9bf110abfbe8f867c6e7ca
John Freeman <john@Johns-MacBook-Pro.local>
2021-11-16 20:20:19 -0500

    final challenge upload with working website

--- scraping.py
+++ scraping.py
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemisphere_image(browser)
     }
 
     # Stop webdriver and return data
